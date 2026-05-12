2f7722f46ce6a383fdabd4ffdeaae0c359b68a30
Xavier Dupré <xadupre@microsoft.com>
2026-05-12 12:07:58 +0200

    update make_doc.sh

--- make_doc.sh
+++ make_doc.sh
@@ -1,4 +1,14 @@
 echo "--"
+echo "-- Builds cpp examples"
+echo "--"
+cd examples
+ONNX_GIT_TAG=v1.21.0 ./build.sh 
+cd ..
+echo "--"
+echo "-- Builds inline"
+echo "--"
+python setup.py build_ext --inplace
+echo "--"
 echo "-- Builds the documentation"
 echo "--"
 python -m sphinx docs dist/html -j auto

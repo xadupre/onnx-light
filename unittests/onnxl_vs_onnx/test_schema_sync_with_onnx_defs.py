24bb72c94c6b115075a0b1a7597f0c67d7779001
Xavier Dupré <xadupre@microsoft.com>
2026-05-16 19:49:15 +0200

    fix

--- unittests/onnxl_vs_onnx/test_schema_sync_with_onnx_defs.py
+++ unittests/onnxl_vs_onnx/test_schema_sync_with_onnx_defs.py
@@ -17,6 +17,45 @@ class TestSchemaSyncWithOnnxDefs(ExtTestCase):
             onnx_light.onnx.defs.onnx_opset_version(), onnx_defs.onnx_opset_version()
         )
 
+    def test_registered_onnx_ops_match_onnx(self):
+        flex_attention_key = ("ai.onnx.preview", "FlexAttention", 1)
+        onnx_light_schema_keys = {
+            (schema.domain, schema.name, schema.since_version)
+            for schema in onnx_light.onnx.defs.get_all_schemas_with_history()
+        }
+        onnx_schema_keys = {
+            (schema.domain, schema.name, schema.since_version)
+            for schema in onnx_defs.get_all_schemas_with_history()
+        }
+
+        if flex_attention_key not in onnx_schema_keys:
+            onnx_light_schema_keys.discard(flex_attention_key)
+
+        self.assertEqual(onnx_light_schema_keys, onnx_schema_keys)
+
+    def test_registered_onnx_ops_match_onnx_match_input_output(self):
+        light_hist = onnx_light.onnx.defs.get_all_schemas_with_history()
+        hist = onnx.defs.get_all_schemas_with_history()
+        self.assertEqual(len(light_hist), len(hist))
+        light_dict = {(s.domain, s.name, s.since_version): s for s in light_hist}
+        onnx_dict = {(s.domain, s.name, s.since_version): s for s in hist}
+        self.assertEqual(set(light_dict), set(onnx_dict))
+        for key, schema in onnx_dict.items():
+            lights = light_dict[key]
+            self.assertEqual(len(schema.inputs), len(lights.inputs))
+            self.assertEqual(len(schema.outputs), len(lights.outputs))
+            self.assertEqual(len(schema.type_constraints), len(lights.type_constraints))
+            self.assertEqual(schema.has_function, lights.has_function)
+            self.assertEqual(schema.deprecated, lights.deprecated)
+            self.assertEqual(schema.is_infinite(0), lights.is_infinite(0))
+            self.assertEqual(schema.support_level, lights.support_level)
+            self.assertEqual(schema.max_input, lights.max_input)
+            self.assertEqual(schema.max_output, lights.max_output)
+            self.assertEqual(schema.file, lights.file)
+            self.assertEqual(schema.line, lights.line)
+            self.assertEqual(schema.non_deterministic, lights.non_deterministic)
+            self.assertEqual(schema.doc, lights.doc)
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)

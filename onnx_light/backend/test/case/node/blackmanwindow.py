d6882e8ecf69a6f9ae105b42c0598c3d8874df96
Xavier Dupré <xadupre@microsoft.com>
2026-05-16 18:05:48 +0200

    style

--- onnx_light/backend/test/case/node/blackmanwindow.py
+++ onnx_light/backend/test/case/node/blackmanwindow.py
@@ -14,24 +14,25 @@ class BlackmanWindow(Base):
     def export() -> None:
         node = onnx.helper.make_node("BlackmanWindow", inputs=["x"], outputs=["y"])
         size = np.int32(10)
-        size_tensor = np.array(size, dtype=np.int32)
         a0 = 0.42
         a1 = -0.5
         a2 = 0.08
-        y = np.array(a0, dtype=np.float32)
-        y += a1 * np.cos(2 * np.pi * np.arange(0, size, 1, dtype=np.float32) / size)
-        y += a2 * np.cos(4 * np.pi * np.arange(0, size, 1, dtype=np.float32) / size)
-        expect(
-            node, inputs=[size_tensor], outputs=[y.astype(np.float32)], name="test_blackmanwindow"
+        y = (
+            a0
+            + a1 * np.cos(2 * np.pi * np.arange(0, size, 1, dtype=np.float32) / size)
+            + a2 * np.cos(4 * np.pi * np.arange(0, size, 1, dtype=np.float32) / size)
         )
+        expect(node, inputs=[size], outputs=[y.astype(np.float32)], name="test_blackmanwindow")
 
         node = onnx.helper.make_node("BlackmanWindow", inputs=["x"], outputs=["y"], periodic=0)
-        y = np.array(a0, dtype=np.float32)
-        y += a1 * np.cos(2 * np.pi * np.arange(0, size, 1, dtype=np.float32) / (size - 1))
-        y += a2 * np.cos(4 * np.pi * np.arange(0, size, 1, dtype=np.float32) / (size - 1))
+        y = (
+            a0
+            + a1 * np.cos(2 * np.pi * np.arange(0, size, 1, dtype=np.float32) / (size - 1))
+            + a2 * np.cos(4 * np.pi * np.arange(0, size, 1, dtype=np.float32) / (size - 1))
+        )
         expect(
             node,
-            inputs=[size_tensor],
+            inputs=[size],
             outputs=[y.astype(np.float32)],
             name="test_blackmanwindow_symmetric",
         )

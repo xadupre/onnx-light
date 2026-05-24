527267c40a5d8b3e515fe69e54e90ccfed84575a
Xavier Dupré <xadupre@microsoft.com>
2026-05-24 09:42:00 +0200

    clean

--- clean.sh
+++ clean.sh
@@ -19,6 +19,7 @@ if [ "$1" == "--all" ]; then
     rm -rf .pytest_cache/
     rm -rf docs/_build/ -rf
     rm -rf docs/auto_* -rf
+    rm -rf docs/operators
     find . -type d -name "__pycache__" -exec rm -rf {} + 2>/dev/null || true
     find . -type d -name "*.egg-info" -exec rm -rf {} + 2>/dev/null || true
 fi
